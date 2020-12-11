19fd28dbc094ceb4f3fa445af2fd060aa898cf00
maximus <1091261998@qq.com>
2020-12-11 14:40:14 +0800

    井字棋游戏基本内容

--- src/index.js
+++ src/index.js
@@ -68,13 +68,20 @@ class Game extends React.Component {
             history: [{
                 squares:Array(9).fill(null),
             }],
+            stepNumber:0,
             xIsNext: true
         };
     }
 
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        })
+    }
+
     handleClick(i) {
-        //创建一个square的副本
-        const history = this.state.history;
+        const history = this.state.history.slice(0,this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if(calculateWinner(squares) || squares[i]) {
@@ -86,13 +93,14 @@ class Game extends React.Component {
             history: history.concat([{
                 squares:squares
             }]),
-            xIsNext: !this.state.xIsNext
+            xIsNext: !this.state.xIsNext,
+            stepNumber: history.length
         })
     }
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         /**
@@ -103,7 +111,7 @@ class Game extends React.Component {
         const moves = history.map((step,move) => {
             const desc = move ? 'Go to move #' + move : 'Go to game start';
             return (
-                <li>
+                <li key={move}>
                     <button onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             )
